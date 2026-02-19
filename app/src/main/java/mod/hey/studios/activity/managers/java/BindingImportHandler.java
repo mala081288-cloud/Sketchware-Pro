a221350b15f40e74fc9763c0afe71bc3547366c0
mala081288-cloud <mala081288@gmail.com>
2026-02-19 21:02:02 +0700

    Add BindingImportHandler implementation

---
+++ app/src/main/java/mod/hey/studios/activity/managers/java/BindingImportHandler.java
@@ -0,0 +1,10 @@
+// BindingImportHandler.java
+
+package mod.hey.studios.activity.managers.java;
+
+public class BindingImportHandler {
+    // Implementation of binding import handler
+    public void handleImports() {
+        // Logic for handling imports
+    }
+}
